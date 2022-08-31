1f1f32bc4f3d5ab22eaa404a7cb3ff4ae58a72e1
Erick Suárez <esuarez_97@hotmail.com>
2022-08-31 18:25:26 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import "bootswatch/dist/spacelab/bootstrap.css";
-import './styles/index.css';
-import App from './App';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App /> );
-
